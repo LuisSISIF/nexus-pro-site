740b63c7e7788b6c6b207979235a690fd41269db
Luís Henrique Freire de Lima <dimitrisouza300@gmail.com>
2025-07-28 17:37:07 +0000

    coloque 3 pontos no email do footer caso exceda o tamanho maximo

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-400" />
-                <span className="text-gray-300">andromedasolutions2025@hotmail.com</span>
+                <span className="text-gray-300 truncate">andromedasolutions2025@hotmail.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-blue-400" />
