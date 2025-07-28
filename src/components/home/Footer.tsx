016ea97ce92069b4a4e3f85a97e7b49f6d98847f
Luís Henrique Freire de Lima <dimitrisouza300@gmail.com>
2025-07-28 17:41:49 +0000

    quando clicar no email abrir o link mail

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-400" />
-                <span className="text-gray-300 truncate">andromedasolutions2025@hotmail.com</span>
+                <a href="mailto:andromedasolutions2025@hotmail.com" className="text-gray-300 truncate hover:text-white transition-colors">andromedasolutions2025@hotmail.com</a>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-blue-400" />
