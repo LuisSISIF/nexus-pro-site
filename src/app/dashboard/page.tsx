5aed9a137c33b005d1f6fd070509700a895a9f79
Luís Henrique Freire de Lima <dimitrisouza300@gmail.com>
2025-07-29 21:02:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import React, 'useEffect', useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Building, DollarSign, Package, Users } from 'lucide-react';
 import {
